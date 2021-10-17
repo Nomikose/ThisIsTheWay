7d5383079713d18b502c5cd99df6b65f52bd15b5
Nomikose <Dartwer55@gmail.com>
2021-10-18 01:07:34 +0300

    Initial commit

---
+++ Task2/IntercalaryYear.py
@@ -0,0 +1,10 @@
+print("Введите год:")
+year = int(input())
+if ((year%4)!=0):
+   print("Not a leap year")
+elif ((year%100)!=0):
+     print("Leap year")
+elif ((year%400)==0):
+     print("Leap year")
+else:
+    print("Not a leap year")
